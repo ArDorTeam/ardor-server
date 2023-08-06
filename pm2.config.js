bd532daf220eec3763f1cf2d0104542b0158f671
ZTli <851186917@qq.com>
2023-08-06 15:15:50 +0800

    chore: init pm2

---
+++ pm2.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps : [
+      {
+        name: "api_v1",
+        script: "./dist/main.js",
+        instances: 'max',
+        instance_var: 'INSTANCE_ID',
+        exec_mode: 'fork'
+      }
+    ]
+  }
