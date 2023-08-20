995be9f2c8181394ea9ffd915db240c8a2103749
Richard <richard@RicharddeMacBook-Air.local>
2023-08-20 16:38:05 +0800

    feat: 注释login文件夹引入

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { ArticleModule } from './modules/article/article.module';
-import { LoginModule } from './modules/login/login.module';
+// import { LoginModule } from './modules/login/login.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, ArticleModule, LoginModule, AuthModule, UsersModule],
+  imports: [PrismaModule, ArticleModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
