406ef09a71829750609ed98628e712ae8b6ea100
Richard <richard@RicharddeMacBook-Air.local>
2023-12-21 23:40:07 +0800

    feat: 更新列表权限

---
+++ PRISMA.md
@@ -0,0 +1,5 @@
+### 每当您对 Prisma 架构中反映的数据库进行更改时，您需要手动重新生成 Prisma 客户端以更新目录中生成的代码node_modules/.prisma/client：
+
+```prisma generate```
+
+### 初始化数据库时需要在根目录生成.env文件
--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -3,12 +3,14 @@ import { ArticleService } from './article.service'
 import { CreateArticleDto, UpdateArticleDto, SearchDto, DeleteArticleDto, getArticleDetailDto } from './dto'
 import { PaginateDto } from 'src/common/dto';
 import { ValidationPipe } from '../../common/pipes/validation/validation.pipe'
+import { GetCurrentUser, GetCurrentUserId, Public } from 'src/common/decorators';
 
 
 @Controller('article')
 export class ArticleController {
     constructor(private articleService:ArticleService) {}
 
+    @Public()
     @Post('/getArticleList')
     async getAll(@Body() paginate: PaginateDto, @Body() searchData: SearchDto) {
         return this.articleService.getArticleList(paginate, searchData)
